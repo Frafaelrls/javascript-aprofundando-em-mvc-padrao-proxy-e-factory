2618972fe65a460a67433ec8b25fa5601e13c3d2
Frafaelrls <frafaelrls@gmail.com>
2023-03-09 15:39:06 -0300

    mudando o contexto de execução da funcao

--- aluraframe/client/js/app/models/ListaNegociacoes.js
+++ aluraframe/client/js/app/models/ListaNegociacoes.js
@@ -1,14 +1,26 @@
 class ListaNegociacoes {
 
     #negociacoes;
+    #armadilha;
+    #contexto;
     
-    constructor() {
+    constructor(contexto, armadilha) {
 
         this.#negociacoes = [];
+        // armadilha recebe uma função
+        this.#armadilha = armadilha;
+        this.#contexto = contexto;
     }
 
     adiciona(negociacao) {
         this.#negociacoes.push(negociacao);
+
+        /*
+            Classe Reflect.apply, é utilizada para modificar o contexto de execução
+            de uma função e dentro de um array é passado os parâmretros que serão usados. 
+        */
+
+        Reflect.apply(this.#armadilha, this.#contexto, [this]);
     }
 
     get negociacoes(){
@@ -17,5 +29,7 @@ class ListaNegociacoes {
 
     esvazia() {
         this.#negociacoes = [];
+
+        Reflect.apply(this.#armadilha, this.#contexto, [this]);
     }
 }
