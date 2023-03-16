df61e602665a62964056bcafc4f87f8e32cd4c18
Frafaelrls <frafaelrls@gmail.com>
2023-03-16 09:42:54 -0300

    adicionando criacao de proxy utlizando a classe ProxyFactory

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -20,67 +20,39 @@ class NegociacaoController {
         this.#inputQuantidade = $('#quantidade');
         this.#inputValor = $('#valor');
         
-        let self = this;
-
-        this.#listaNegociacoes = new Proxy(new ListaNegociacoes(), {
-
-            get(target, prop, receiver) {
-
-                /*
-                    Verificando se a propriedade enviada é um dos dois métodos e se é uma função
-                    Caso verdadeiro, será retornado uma função
-                */
-
-                if(['adiciona', 'esvazia'].includes(prop) && typeof(target[prop]) == typeof(Function)) {
-                    
-                    return function() {
-                        console.log(`Interceptando ${prop}`);
-
-                        /*
-                          O Reflect faz com que o método alvo receba os parâmetros (arguments) enviados
-                          e executa o método dentro do seu contexto (target)  
-
-                        */
-
-                        Reflect.apply(target[prop], target, arguments);
-                        self.#negociacoesView.update(target);
-                    }
-                }
-
-                return target[prop];
-                
-            }
-        });
-      
+        this.#listaNegociacoes = ProxyFactory.create(
+            new ListaNegociacoes(), 
+            ['adiciona', 'esvazia'], model =>
+                this.#negociacoesView.update(model));      
        
         // NegociacoesView recebe o local onde deve ser incluida no DOM
         this.#negociacoesView = new NegociacoesView($('#negociacoesView'));
         this.#negociacoesView.update(this.#listaNegociacoes);
 
-        this.#mensagem = new Mensagem();
+        this.#mensagem = ProxyFactory.create( 
+            new Mensagem(), ['texto'], model =>
+                this.#mensagemView.update(model));
+
         // MensagemView recebe o local onde deve ser incluida no DOM
-        this.#mensagemView = new MensagemView($('#mensagemView'));
-        this.#mensagemView.update(this.#mensagem);
+        this.#mensagemView = new MensagemView($('#mensagemView'));  
+        this.#mensagemView.update(this.#mensagem);  
 
     }
 
     adiciona(event) {
         event.preventDefault();
-
+        
         this.#listaNegociacoes.adiciona(this.#criaNegociacao());
-
-        this.#mensagem.texto = 'Negociação adicionada com sucesso!';
-        this.#mensagemView.update(this.#mensagem);
-       
+        this.#mensagem.texto = 'Negociação adicionada com sucesso!';      
         this.#limpaFormulario();
 
     };
 
     apaga() {
-        this.#listaNegociacoes.esvazia();
 
+        this.#listaNegociacoes.esvazia();
         this.#mensagem.texto = 'Negociações apagadas com sucesso';
-        this.#mensagemView.update(this.#mensagem);
+
     }
 
 
