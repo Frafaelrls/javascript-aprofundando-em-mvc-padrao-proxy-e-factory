10bd477a553952ceed95c7668250c0b6bcf676d1
Frafaelrls <frafaelrls@gmail.com>
2023-03-16 12:04:56 -0300

    adicionando o uso do REST operator no parametro ...props

--- aluraframe/client/js/app/helpers/Bind.js
+++ aluraframe/client/js/app/helpers/Bind.js
@@ -1,5 +1,14 @@
 class Bind { 
-    constructor(model, view, props) {
+
+    /*
+        Utilizando três pontos afrente de um a parâmetro estamos 
+        Transformando-o em um REST operator. Assim, todos os parâmetros passados
+        Serão adicionados a um array
+
+        O REST operator deve ser o último parâmetro.
+    */
+
+    constructor(model, view, ...props) {
 
         let proxy = ProxyFactory.create(model, props, model => 
             view.update(model));
