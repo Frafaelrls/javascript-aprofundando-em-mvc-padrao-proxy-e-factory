0412407cb7e9310cf0fdfa4420a5cc23ecbb4adb
Frafaelrls <frafaelrls@gmail.com>
2023-03-31 16:01:05 -0300

    criando o arquivo NegociacaoService

---
+++ aluraframe/client/js/app/services/NegociacaoService.js
@@ -0,0 +1,38 @@
+class NegociacaoService {
+
+    obeterNegociacoesDaSemana(cb) {
+
+        let xhr = new XMLHttpRequest();
+
+        xhr.open('GET', 'negociacoes/semana');
+
+        /*
+            Estado da requisição ajax:
+            0: requisição ainda não iniciada
+            1: conexão com o servidor estabelecida
+            2: requisição recebida
+            3: processando requisição
+            4: requisição está concluída e a resposta está pronta
+        */
+
+        // Quando o estado mudar a função é executada 
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState == 4) {
+
+                if (xhr.status == 200) {
+                    // transformando o JSON em objetos 
+                    cb(null, JSON.parse(xhr.responseText)
+                        .map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor)));
+
+
+                } else {
+                    console.log(xhr.responseText);
+                    cb('Não foi possível obeter as negociações', null);
+
+                }
+            }
+        };
+
+        xhr.send();
+    }
+}
